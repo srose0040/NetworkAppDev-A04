ec6aee3f69a57b3d3a1c3fae8054b480bad671f4
srose0040 <115491189+srose0040@users.noreply.github.com>
2024-03-26 19:39:49 -0400

    Conducted first simple ajax call

--- nad-a04/src/posts/views.py
+++ nad-a04/src/posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Posts
+from django.http import JsonResponse
 # Create your views here.
 
 def post_list_and_create(request):
     qs = Posts.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'hello world'})
