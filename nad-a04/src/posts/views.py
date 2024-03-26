97773041cec9098267ed069427bb717ff06c8f19
srose0040 <115491189+srose0040@users.noreply.github.com>
2024-03-26 19:27:22 -0400

    Ading first view and template

--- nad-a04/src/posts/views.py
+++ nad-a04/src/posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Posts
 # Create your views here.
+
+def post_list_and_create(request):
+    qs = Posts.objects.all()
+    return render(request, 'posts/main.html', {'qs':qs})
