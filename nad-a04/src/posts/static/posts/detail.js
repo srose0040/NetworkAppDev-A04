365317ce40ffd526161a6f023a9fb78857e2b4a5
srose0040 <115491189+srose0040@users.noreply.github.com>
2024-04-06 15:49:56 -0400

    Modyfying buttons for post detail page

--- nad-a04/src/posts/static/posts/detail.js
+++ nad-a04/src/posts/static/posts/detail.js
@@ -1,6 +1,8 @@
 console.log('hello-world-detail')
 
 const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('delete-btn')
+const deleteBtn = document.getElementById('update-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
@@ -13,6 +15,15 @@ $.ajax({
     url: url,
     success: function(response){
         console.log(response)
+        const data = response.data
+        if (data.logged_in !== data.author){
+            console.log('diffent')
+        } else {
+            console.log('thesame')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
