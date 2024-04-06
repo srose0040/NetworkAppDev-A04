e7311e54142bbe7ff10c729f467c8e3a26915659
srose0040 <115491189+srose0040@users.noreply.github.com>
2024-04-05 20:15:52 -0400

    Finished creating posts with ajax

--- nad-a04/src/posts/views.py
+++ nad-a04/src/posts/views.py
@@ -14,29 +14,36 @@ def post_list_and_create(request):
             instance = form.save(commit=False)
             instance.author = author
             instance.save()
+            return JsonResponse({
+                'title': instance.title,
+                'body': instance.body,
+                'author': instance.author.user.username,
+                'id': instance.id,
+            })
     context = {
         'form': form,
     }
     return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
-    visible = 3
-    upper = num_posts
-    lower = upper - visible
-    size = Posts.objects.all().count()
+    if request.is_ajax():
+        visible = 3
+        upper = num_posts
+        lower = upper - visible
+        size = Posts.objects.all().count()
 
-    qs = Posts.objects.all()
-    data = []
-    for obj in qs:
-        item = {
-            'id': obj.id,
-            'title': obj.title,
-            'body': obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
-            'count': obj.like_count,
-            'author': obj.author.user.username
-        }
-        data.append(item)
+        qs = Posts.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author': obj.author.user.username
+            }
+            data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size': size})
 
 def like_unlike_post(request):
