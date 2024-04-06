8c4eb4c90ee904d5e9cd5f076b5720efe034bc47
srose0040 <115491189+srose0040@users.noreply.github.com>
2024-04-06 18:04:47 -0400

    Created view for image uploading

--- nad-a04/src/posts/urls.py
+++ nad-a04/src/posts/urls.py
@@ -6,7 +6,8 @@ from .views import(
     post_detail,
     post_detail_data_view, 
     delete_post,
-    update_post
+    update_post,
+    image_upload_view
 )
 
 app_name = 'posts'
@@ -15,6 +16,7 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='posts-data'),
+    path('upload/', image_upload_view, name='image-upload'),
     path('<pk>/', post_detail, name='post-detail'),
     path('<pk>/update/', update_post, name='post-update'),
     path('<pk>/delete/', delete_post, name='post-delete'),
